c79bab36cc4918fe52f443e3504e6190531c19f8
tools <sasamikasa21@gmail.com>
2024-09-24 06:55:21 +0700

    Update hack.sh

--- hack.sh
+++ hack.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 clear
+
+
+
+
 echo
       echo -e "[+] ________________________________"
       echo -e "[+] author : EXPLOIT INDEPENDENT [+]"
       echo -e "[+] TEAM   : ANAK GABUT          [+]"
       echo -e "[+] ________________________________"
 echo
+echo""
            echo -e "[1]. TOOLS GESER MATAHARI"
            echo -e "[2]. TOOLS GESER BULAN"
            echo -e "[3]. TOOLS GESER BUMI"
            echo -e "[4]. INSTALL BAHAN DULU TOD"
+           echo -e "[5]. exit tod"
            echo ""
 read -p "pilih : " pilih
 if [ $pilih = "1" ]
 then
         echo "menginstall......."
         sleep 2
-          
-      
+        apt update
 clear
         echo""
         echo "sedang scaning......"
@@ -63,7 +68,20 @@ then
       sleep 2 
 clear
       echo "sudah terinstall...."
+    
       echo ""
+      
+elif [ $pilih = "5" ]
+then
+      echo ""
+      echo "THANK YOU TOD........"
+      sleep 4
+      clear
+      echo "JADI ORANG JANGAN TOLOL......"
+      echo ""
+      sleep 5
+      clear
+      exit
         
          
 fi
