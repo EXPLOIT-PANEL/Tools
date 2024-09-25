4d1f4a7d637476b45330513337532d1f1077dde4
tools <sasamikasa21@gmail.com>
2024-09-25 10:57:01 +0700

    Add files via upload

---
+++ Belajar.sh
@@ -0,0 +1,270 @@
+#!/bin/bash
+clear
+echo
+
+echo -e "AUTHOR :
+███████╗██╗░░██╗██████╗░██╗░░░░░░█████╗░██╗████████╗
+██╔════╝╚██╗██╔╝██╔══██╗██║░░░░░██╔══██╗██║╚══██╔══╝
+█████╗░░░╚███╔╝░██████╔╝██║░░░░░██║░░██║██║░░░██║░░░
+██╔══╝░░░██╔██╗░██╔═══╝░██║░░░░░██║░░██║██║░░░██║░░░
+███████╗██╔╝╚██╗██║░░░░░███████╗╚█████╔╝██║░░░██║░░░
+╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚══════╝░╚════╝░╚═╝░░░╚═╝░░░" 
+
+sleep 3
+echo""
+clear
+
+echo -e "install bahan dulu ganteng ......"
+echo""
+        
+echo "sebelum install bahan nya saya 
+berterimakasih sudah menajalankan
+tools tolol ini soalnya masih pemula"
+echo "____________________________________"
+ sleep 5
+    
+ 
+ clear
+ echo""
+ echo "install......"
+ clear
+ echo "done"
+
+        sleep 1 
+        clear
+        
+  clear
+ echo""
+ echo
+
+  
+  
+  
+echo -e "AUTHOR :
+███████╗██╗░░██╗██████╗░██╗░░░░░░█████╗░██╗████████╗
+██╔════╝╚██╗██╔╝██╔══██╗██║░░░░░██╔══██╗██║╚══██╔══╝
+█████╗░░░╚███╔╝░██████╔╝██║░░░░░██║░░██║██║░░░██║░░░
+██╔══╝░░░██╔██╗░██╔═══╝░██║░░░░░██║░░██║██║░░░██║░░░
+███████╗██╔╝╚██╗██║░░░░░███████╗╚█████╔╝██║░░░██║░░░
+╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚══════╝░╚════╝░╚═╝░░░╚═╝░░░" 
+
+echo -e "TEAM : TUKANG ATTACK"
+
+echo""
+        echo -e "[1]. RANSOMEWARE (ROOT)"
+        echo -e "[2]. OSINT NIK (EROR)"
+        echo -e "[3]. BOT WA GACOR"
+        echo -e "[4]. SEEKER"
+        echo -e "[5]. phiser"
+        echo -e "[6]. spam "
+        echo -e "[7]. CEK KHODAM PENDAMPING"
+        echo -e "[8]. TAMPILAN TERMUX"
+        echo -e "[9]. exit tod"
+        
+        echo ""
+        
+           
+read -p "pilih : " pilih
+if [ $pilih = "1" ]
+then
+        echo "menginstall......."
+        sleep 2
+        git clone https://github.com/termuxhackers-id/SARA && cd SARA && sudo bash install.sh && 
+exit
+
+
+        python3 sara.py
+clear
+        echo""
+        echo "sedang scaning......"
+        sleep 2
+        echo""
+clear
+        echo "yess berhasil......."
+        sleep 3 
+        clear
+        echo""
+        echo "ketik ls ya tod"
+        sleep 10
+        
+elif [ $pilih = "2" ]
+then
+         echo "menginstall......"
+         echo""
+         clear
+         echo "toolsnya mokad"
+         sleep 5
+exit
+         
+clear
+         
+        echo""
+        echo "sudah berhasil....."
+        sleep 3 
+    
+    
+        echo""
+        
+elif [ $pilih = "3" ]
+then
+   
+        
+        echo "menginstall......."
+        sleep 3 
+        apt update
+        apt upgrade
+        pkg update && pkg upgrade
+        pkg install bash
+        pkg install libwebp
+        pkg install git -y
+        pkg install nodejs -y 
+        pkg install ffmpeg -y 
+        pkg install wget
+        pkg install imagemagick -y
+clear
+        git clone https://github.com/DGXeon/CheemsBot-MD11
+        cd CheemsBot-MD11
+        yarn install
+        npm start
+clear
+        echo""
+elif [ $pilih = "4" ] 
+then 
+      echo "menginstall......"
+      sleep 3 
+      
+        git clone https://github.com/thewhiteh4t/seeker.git
+        clear
+        cd seeker/
+        chmod +x install.sh
+        ./install.sh
+      
+      clear
+      echo""
+      echo "bahan sudah terinstall ketik ls"
+      echo""
+      
+elif [ $pilih = "5" ] 
+then 
+        echo "menginstall....."
+        sleep 3 
+        
+        git clone --depth=1 https://github.com/htr-tech/zphisher.git 
+        clear
+        
+      echo "ketik ls ya tod"
+      echo""
+elif [ $pilih = "6" ]
+then 
+      
+        echo "menginstall....."
+        sleep 3 
+        echo
+        clear
+        
+
+        pkg update
+        pkg upgrade
+        apt install curl git ruby
+        apt install make clang
+        apt install screen
+        apt install python
+        apt install python-pip mpv
+        pip install rich
+        pip install rich-cli
+        gem install lolcat
+
+clear
+        git clone https://github.com/Bayu12345677/Ketupat-Terror.git
+clear
+        
+        
+clear
+        echo""
+        echo "ketik ls tod"
+        sleep 4 
+        echo""
+    
+elif [ $pilih = "7" ]
+then
+         
+         echo "scaning mode........"
+         sleep 3 
+         clear
+         echo "sabar tod dukun nya masih sarapan"
+         sleep 5 
+         
+clear
+        echo""
+        echo""
+        echo "+"
+        sleep 2 
+      
+        echo "++"
+        sleep 2 
+        echo "+++"
+        sleep 2 
+        echo "++++"
+        sleep 2 
+        echo""
+        echo "KHODAM KAMU :
+           ██████╗░░█████╗░██████╗░██╗
+           ██╔══██╗██╔══██╗██╔══██╗██║
+           ██████╦╝███████║██████╦╝██║
+           ██╔══██╗██╔══██║██╔══██╗██║
+           ██████╦╝██║░░██║██████╦╝██║
+           ╚═════╝░╚═╝░░╚═╝╚═════╝░╚═╝"
+      
+      
+        sleep 10
+        
+        clear
+
+elif [ $pilih = "8" ]
+then 
+       
+         clear
+         echo "menginstall...."
+         sleep 3 
+         clear
+          
+          git clone https://github.com/remo7777/T-Header.git 
+clear
+           echo "ketik ls tod"
+           sleep 2 
+
+elif [ $pilih = "9" ]
+then
+       clear
+        echo
+        echo "
+████████╗██╗░░██╗░█████╗░███╗░░██╗██╗░░██╗
+╚══██╔══╝██║░░██║██╔══██╗████╗░██║██║░██╔╝
+░░░██║░░░███████║███████║██╔██╗██║█████═╝░
+░░░██║░░░██╔══██║██╔══██║██║╚████║██╔═██╗░
+░░░██║░░░██║░░██║██║░░██║██║░╚███║██║░╚██╗
+░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝
+
+██╗░░░██╗░█████╗░██╗░░░██╗
+╚██╗░██╔╝██╔══██╗██║░░░██║
+░╚████╔╝░██║░░██║██║░░░██║
+░░╚██╔╝░░██║░░██║██║░░░██║
+░░░██║░░░╚█████╔╝╚██████╔╝
+░░░╚═╝░░░░╚════╝░░╚═════╝░"
+
+        sleep 3 
+        echo""
+        exit
+
+         
+fi
+  
+
+
+        
+         
+        
+        
+        
+
+
