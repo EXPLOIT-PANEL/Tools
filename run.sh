4a5914b39baa6ae790b861a5d83525978281d3c2
tools <sasamikasa21@gmail.com>
2024-09-25 11:07:30 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 apt update && apt upgrade -y
 apt install figlet
-apt install cawsay
+apt install cowsay
 apt install bash
 apt install git
 apt install python
 apt install ruby
 apt install nala
+apt install toilet
