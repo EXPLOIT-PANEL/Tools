7957976cc22b57de18657f6720c53f51c6d05048
tools <sasamikasa21@gmail.com>
2024-09-26 17:57:44 +0700

    Add files via upload

---
+++ Belajar.sh
@@ -0,0 +1,276 @@
+#!/bin/bash 
+
+# Color variables
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+magenta='\033[0;35m'
+cyan='\033[0;36m'
+# Clear the color after that
+clear='\033[0m'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+clear
+echo
+
+echo -e "${green} AUTHOR :
+███████╗██╗░░██╗██████╗░██╗░░░░░░█████╗░██╗████████╗
+██╔════╝╚██╗██╔╝██╔══██╗██║░░░░░██╔══██╗██║╚══██╔══╝
+█████╗░░░╚███╔╝░██████╔╝██║░░░░░██║░░██║██║░░░██║░░░
+██╔══╝░░░██╔██╗░██╔═══╝░██║░░░░░██║░░██║██║░░░██║░░░
+███████╗██╔╝╚██╗██║░░░░░███████╗╚█████╔╝██║░░░██║░░░
+╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚══════╝░╚════╝░╚═╝░░░╚═╝░░░" 
+
+
+sleep 3
+echo""
+clear
+
+echo -e "install bahan dulu ganteng ......"
+echo""
+        
+echo "sebelum install bahan nya saya 
+berterimakasih sudah menajalankan
+tools tolol ini soalnya masih pemula"
+echo "____________________________________"
+ mpv hacker.mp3>clear 
+ 
+    
+ 
+ 
+ 
+ 
+        sleep 3 
+        clear
+        
+  clear
+ echo""
+ echo
+
+ 
+mpv dj1.mp3>clear -&
+
+
+echo""
+echo "SUDAH TERINSTALL DENGAN BAIK"
+sleep 4 
+clear
+  
+  
+echo -e "AUTHOR :
+███████╗██╗░░██╗██████╗░██╗░░░░░░█████╗░██╗████████╗
+██╔════╝╚██╗██╔╝██╔══██╗██║░░░░░██╔══██╗██║╚══██╔══╝
+█████╗░░░╚███╔╝░██████╔╝██║░░░░░██║░░██║██║░░░██║░░░
+██╔══╝░░░██╔██╗░██╔═══╝░██║░░░░░██║░░██║██║░░░██║░░░
+███████╗██╔╝╚██╗██║░░░░░███████╗╚█████╔╝██║░░░██║░░░
+╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚══════╝░╚════╝░╚═╝░░░╚═╝░░░" 
+
+echo -e "TEAM : TUKANG ATTACK"
+sleep 3 
+
+
+echo""
+        echo -e "[1]. RANSOMEWARE (ROOT)"
+        echo -e "[2]. OSINT NIK (pembaruan)"
+        echo -e "[3]. BOT WA GACOR"
+        echo -e "[4]. SEEKER"
+        echo -e "[5]. phiser"
+        echo -e "[6]. PHONE XPLOIT"
+        echo -e "[7]. CEK KHODAM PENDAMPING"
+        echo -e "[8]. TAMPILAN TERMUX"
+        echo -e "[9]. exit tod"
+        
+        echo ""
+      
+    
+        
+      
+        
+      
+           
+read -p "pilih : " pilih
+if [ $pilih = "1" ]
+then
+        echo "menginstall......."
+        sleep 2
+        cd SARA &&
+        
+        python3 sara.py
+exit
+
+
+        
+elif [ $pilih = "2" ]
+then
+         echo "menginstall......"
+         echo""
+         clear
+         echo "toolsnya mokad"
+         sleep 5
+exit
+      
+
+        
+elif [ $pilih = "3" ]
+then
+   
+        
+        echo "menginstall......."
+        
+        cd CheemsBot-MD11
+        yarn install
+        npm start
+exit
+        
+elif [ $pilih = "4" ] 
+then 
+      echo "menginstall......"
+      sleep 2 
+      clear 
+        
+        
+        cd seeker && python3 seeker.py 
+        
+        
+        clear
+        
+      
+    
+    
+      
+elif [ $pilih = "5" ] 
+then 
+        echo "menginstall....."
+        sleep 3 
+        
+        
+        clear
+        
+      cd zphisher
+       bash zphisher.sh
+exit
+      
+elif [ $pilih = "6" ]
+then 
+      
+        echo "sabar"
+        sleep 1
+        echo
+        clear
+        
+ 
+cd PhoneSploit-Pro/
+python3 phonesploitpro.py
+
+  
+exit
+    
+elif [ $pilih = "7" ]
+then
+         
+         echo "scaning mode........"
+         sleep 3 
+         clear
+         echo "sabar tod dukun nya masih sarapan"
+         sleep 5 
+         
+clear
+        echo""
+        echo -ne '••••                     (33%)\r'
+        sleep 2
+        echo -ne '••••••••                 (66%)\r'
+        sleep 3 
+        echo -ne '••••••••••••••••••••••  (100%)\r'
+        echo -ne '\n'
+        clear 
+        
+        echo -e " ${red}KHODAM KAMU :
+           ██████╗░░█████╗░██████╗░██╗
+           ██╔══██╗██╔══██╗██╔══██╗██║
+           ██████╦╝███████║██████╦╝██║
+           ██╔══██╗██╔══██║██╔══██╗██║
+           ██████╦╝██║░░██║██████╦╝██║
+           ╚═════╝░╚═╝░░╚═╝╚═════╝░${clear}
+"                    
+echo "LU SERING PEGANG LILIN YA TOD"
+mpv meme.mp3>clear
+      
+        sleep 2 
+        
+        clear
+        
+
+elif [ $pilih = "8" ]
+then 
+       
+         clear
+         echo "menginstall...."
+         sleep 3 
+         clear
+          cd T-Header/
+          
+          bash t-header.sh
+          
+clear
+           echo "hehehhe"
+           sleep 2 
+
+elif [ $pilih = "9" ]
+then
+       clear
+        echo
+        echo "
+████████╗██╗░░██╗░█████╗░███╗░░██╗██╗░░██╗
+╚══██╔══╝██║░░██║██╔══██╗████╗░██║██║░██╔╝
+░░░██║░░░███████║███████║██╔██╗██║█████═╝░
+░░░██║░░░██╔══██║██╔══██║██║╚████║██╔═██╗░
+░░░██║░░░██║░░██║██║░░██║██║░╚███║██║░╚██╗
+░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝
+
+██╗░░░██╗░█████╗░██╗░░░██╗
+╚██╗░██╔╝██╔══██╗██║░░░██║
+░╚████╔╝░██║░░██║██║░░░██║
+░░╚██╔╝░░██║░░██║██║░░░██║
+░░░██║░░░╚█████╔╝╚██████╔╝
+░░░╚═╝░░░░╚════╝░░╚═════╝░"  
+
+        sleep 3 
+        echo""
+       
+       
+       
+        exit
+        
+   
+     
+     
+     
+     
+         
+fi
+  
+
+
+        
+         
+        
+        
+        
+
+
---
+++ run.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+echo""
+echo""
+echo "INSTALL BAHAN NYA GANTENG"
+pkg install sox 
+pkg install mpv
+
+
+sleep 2
+        apt update
+        apt upgrade
+        pkg update && pkg upgrade
+        pkg install bash
+        pkg install libwebp
+        pkg install git -y
+        pkg install nodejs -y 
+        pkg install ffmpeg -y 
+        pkg install wget
+        pkg install imagemagick -y
+clear
+        git clone https://github.com/DGXeon/CheemsBot-MD11
+clear
+        git clone https://github.com/thewhiteh4t/seeker.git && bash install.sh 
+clear
+
+        git clone --depth=1 https://github.com/htr-tech/zphisher.git 
+clear
+        pkg update
+        pkg upgrade
+        apt install curl git ruby
+        apt install make clang
+        apt install screen
+        apt install python
+        apt install python-pip mpv
+        pip install rich
+        pip install rich-cli
+        gem install lolcat
+
+
+
+clear
+        git clone https://github.com/remo7777/T-Header.git 
+clear
+     
+
+
+git clone https://github.com/termuxhackers-id/SARA && bash install.sh
+clear
+git clone https://github.com/AzeemIdrisi/PhoneSploit-Pro.git
+cd PhoneSploit-Pro/
+pip install -r requirements.txt
+clear
+
+echo "done sayang......"
+      sleep 2 
+
+
+
+
+
