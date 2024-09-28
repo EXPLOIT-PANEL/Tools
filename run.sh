5873dfbea5ec8d503619488ebae4156b84d4d728
tools <sasamikasa21@gmail.com>
2024-09-28 15:27:11 +0700

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+echo""
+echo""
+echo "INSTALL BAHAN NYA GANTENG"
+echo""
+echo""
+mpv bom.mp3>clear -& 
+pkg install sox 
+pkg install mpv
+sleep 2
+        apt update
+        apt upgrade
+        
+        pkg install bash
+        pkg install libwebp
+        pkg install git -y
+        pkg install nodejs -y 
+        pkg install ffmpeg -y 
+        pkg install wget
+        pkg install imagemagick -y
+clear        
+        
+        apt install curl git ruby
+        apt install make clang
+        apt install screen
+        apt install python
+        apt install python-pip mpv
+        pip install rich
+        pip install rich-cli
+        gem install lolcat
+        apt update
+        
+        clear
+echo "INSTALL"
+
+clear
+git clone https://github.com/DGXeon/CheemsBot-MD11
+clear
+git clone https://github.com/thewhiteh4t/seeker.git && bash install.sh 
+clear
+git clone --depth=1 https://github.com/htr-tech/zphisher.git 
+clear
+git clone https://github.com/remo7777/T-Header.git 
+clear
+git clone https://github.com/termuxhackers-id/SARA && sudo bash install.sh 
+clear
+git clone https://github.com/AzeemIdrisi/PhoneSploit-Pro.git
+cd PhoneSploit-Pro/
+pip install -r requirements.txt
+clear
+git clone https://github.com/pembriahmad/DDOS
+clear
+echo "done sayang......"
+      sleep 2 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
+        
+        
+        
+        
+
+
+
+
+
